d85c6be2d3ef09ac2e0007baf09957e429534eba
mib1092 <iigore4ekk@gmail.com>
2016-11-26 10:23:43 +0200

    create Form Validation (controller, model, view)

---
+++ models/Custom.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use app\models\User;
+
+class Custom extends Model
+{
+    public $name;
+    public $email;
+    public $subject;
+    public $body;
+    public $url;
+    public $age;
+    public $verifyCode;
+    public $pass;
+
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'subject', 'body', 'pass', 'verifyCode'], 'required'],
+            ['name', 'match', 'pattern' => '/^[a-z,A-Z]\w*$/i'],
+            ['email', 'email'],
+            [['name', 'email', 'pass'], 'trim'],
+            ['subject', 'string', 'length' => [3, 100]],
+            ['body', 'string'],
+            ['body', 'safe'],
+            ['url', 'url', 'defaultScheme' => 'http'],
+            ['age', 'integer', 'min' => 5],
+            ['verifyCode', 'captcha'],
+            ['pass', 'passRules'],
+        ];
+    }
+
+    public function passRules()
+    {
+        $user = User::findByUsername($this->username);
+
+        if (!$user || !$user->validatePassword($this->password)) {
+            $this->addError('pass', 'Error!');
+        }
+    }
+}
